4792e757956951524cea33da61249c6a173e612c
Oliver Gosling <oliver.gosling@bigfan.agency>
2019-08-14 20:22:03 +0100

    add browserSync, fix asset paths

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const rename = require('gulp-rename');
 const sass  = require('gulp-sass');
 const autoprefixer  = require('gulp-autoprefixer');
+const browsersync = require("browser-sync").create();
 
 function styles() {
   return gulp.src('scss/style.scss')
@@ -15,11 +16,27 @@ function styles() {
 };
 
 function watchFiles(){
-  gulp.watch(['scss/**/*.scss'], styles);
+  gulp.watch(['scss/**/*.scss', '**/*.html'], gulp.series(styles, browserSyncReload));
+}
+
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./"
+    },
+    port: 3000
+  });
+  done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
 }
 
 const build = gulp.series(styles);
-const watch = gulp.series(watchFiles);
+const watch = gulp.parallel(watchFiles, browserSync);
 
 exports.build = build;
 exports.watch = watch;
