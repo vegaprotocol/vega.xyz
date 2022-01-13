c9bd3bc95b1809bfa986200d5c1423024c369800
Oliver Gosling <oli@big.fan>
2022-01-13 11:26:16 +0000

    update leading line component

--- src/components/LeadingLine.js
+++ src/components/LeadingLine.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const LeadingLine = ({ text }) => {
-  return <p className="font-glitched copy-xs md:copy-s">{text}</p>;
+const LeadingLine = (props) => {
+  return (
+    <p
+      className={`font-glitched copy-xs md:copy-s ${
+        props.className ? props.className : ""
+      }`}
+    >
+      {props.children}
+    </p>
+  );
 };
 
 export default LeadingLine;
