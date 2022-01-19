cd1910de8908420f76519cd2b0078edc21fbc048
Oliver Gosling <oli@big.fan>
2022-01-19 10:11:04 +0000

    include planet/rocket graphic on home page

---
+++ src/components/Svg/PlanetRocket.js
@@ -0,0 +1,216 @@
+import React from "react";
+import Moshed from "../../video/moshed.mp4";
+
+const PlanetRocket = ({ className }) => {
+  return (
+    <div
+      className={`pb-[148.2100239%] relative w-full ${
+        className ? className : ""
+      }`}
+    >
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="absolute left-0 top-0 w-full h-full object-cover"
+      >
+        <source src={Moshed} type="video/mp4" />
+      </video>
+
+      <svg
+        version="1.1"
+        id="Layer_1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        viewBox="0 0 419 621"
+        xmlSpace="preserve"
+        className="absolute left-0 right-0 top-0 h-auto"
+      >
+        <path
+          d="M0,0v621h419V101.5c0,23.5-19,42.5-42.5,42.5S334,125,334,101.5S353,59,376.5,59S419,78,419,101.5V0H0z M383,378
+	c0,102.2-82.8,185-185,185S13,480.2,13,378s82.8-185,185-185S383,275.8,383,378z"
+          className="fill-white dark:fill-black"
+        />
+        <g className="fill-vega-yellow">
+          <path d="M194,45h-3v3h3V45z" />
+          <path d="M194,54h3v3h3v-3h3v-6h-9V54z" />
+          <path d="M206,45h-3v3h3V45z" />
+          <path d="M197,42h-1h-1h-1v3h3V42z" />
+          <path d="M203,42h-1h-1h-1v3h3V42z" />
+        </g>
+        <g className="dark:fill-white fill-black">
+          <g>
+            <rect x="197" y="72" width="3" height="3" />
+            <rect x="197" y="63" width="3" height="3" />
+            <rect x="197" y="90" width="3" height="3" />
+            <rect x="197" y="81" width="3" height="3" />
+            <rect x="197" y="108" width="3" height="3" />
+            <rect x="197" y="99" width="3" height="3" />
+            <rect x="197" y="126" width="3" height="3" />
+            <rect x="197" y="117" width="3" height="3" />
+            <rect x="197" y="144" width="3" height="3" />
+            <rect x="197" y="135" width="3" height="3" />
+            <rect x="197" y="162" width="3" height="3" />
+            <rect x="197" y="153" width="3" height="3" />
+            <rect x="197" y="180" width="3" height="3" />
+            <rect x="197" y="171" width="3" height="3" />
+            <rect x="197" y="189" width="3" height="3" />
+          </g>
+          <path d="M313.6,336.8h-3.2v3.2h3.2V336.8z" />
+          <path d="M316.8,333.6v-3.2h-3.2v3.2v3.2h3.2V333.6z" />
+          <path d="M310.4,339.9H304v3.2h6.4V339.9z" />
+          <path d="M313.6,343.1h-3.2v3.2h3.2V343.1z" />
+          <path d="M316.8,346.3h-3.2v6.4h3.2V346.3z" />
+          <path d="M319.9,343.1h-3.2v3.2h3.2V343.1z" />
+          <path d="M326.3,339.9h-6.4v3.2h6.4V339.9z" />
+          <path d="M319.9,336.8h-3.2v3.2h3.2V336.8z" />
+          <path d="M329.3,327.2h-3.2v3.2h3.2V327.2z" />
+          <path d="M332.5,324h-3.2v3.2h3.2V324z" />
+          <path d="M335.6,320.8h-3.2v3.2h3.2V320.8z" />
+          <path d="M338.8,317.6h-3.2v3.2h3.2V317.6z" />
+          <path d="M342,314.4h-3.2v3.2h3.2V314.4z" />
+          <path d="M345.2,311.3H342v3.2h3.2V311.3z" />
+          <path d="M348.4,308.1h-3.2v3.2h3.2V308.1z" />
+          <path d="M351.6,304.9h-3.2v3.2h3.2V304.9z" />
+          <path d="M354.8,301.7h-3.2v3.2h3.2V301.7z" />
+          <path d="M358,298.5h-3.2v3.2h3.2V298.5z" />
+          <path d="M361.1,295.3H358v3.2h3.2v-3.2H361.1z" />
+          <path d="M364.3,292.1h-3.2v3.2h3.2V292.1z" />
+          <path d="M367.5,288.9h-3.2v3.2h3.2V288.9z" />
+          <path d="M370.7,285.8h-3.2v3.2h3.2V285.8z" />
+          <path d="M373.9,282.6h-3.2v3.2h3.2V282.6z" />
+          <path d="M377.1,279.4h-3.2v3.2h3.2V279.4z" />
+          <path d="M380.3,276.2h-3.2v3.2h3.2V276.2z" />
+          <path d="M383.5,273h-3.2v3.2h3.2V273z" />
+          <path d="M200,0h-3v6h3V0z" />
+          <path d="M197,6h-3v3h3V6z" />
+          <path d="M203,6h-3v3h3V6z" />
+          <path d="M194,33V9h-3v24h-3v3h3v3h3v-3V33z" />
+          <path d="M209,36v-3h-3V9h-3v24v3v3h3v-3H209z" />
+          <path d="M188,36h-3v3h-3v3h9v-3h-3V36z" />
+          <path d="M212,39v-3h-3v3h-3v3h3h3h3v-3H212z" />
+          <path d="M200,33h-3v6h-3v3h3v3h3v-3h3v-3h-3V33z" />
+          <path d="M16.2,480.6H13v3.2h3.2V480.6z" />
+          <path d="M19.4,477.4h-3.2v3.2h3.2V477.4z" />
+          <path d="M22.6,474.2h-3.2v3.2h3.2V474.2z" />
+          <path d="M25.8,471h-3.2v3.2h3.2V471z" />
+          <path d="M28.9,467.8h-3.2v3.2h3.2V467.8z" />
+          <path d="M32.1,464.6h-3.2v3.2h3.2V464.6z" />
+          <path d="M35.3,461.4h-3.2v3.2h3.2V461.4z" />
+          <path d="M38.5,458.3h-3.2v3.2h3.2V458.3z" />
+          <path d="M41.7,455.1h-3.2v3.2h3.2V455.1z" />
+          <path d="M44.9,451.9h-3.2v3.2h3.2V451.9z" />
+          <path d="M48.1,448.7h-3.2v3.2h3.2V448.7z" />
+          <path d="M51.3,445.5h-3.2v3.2h3.2V445.5z" />
+          <path d="M54.4,442.3h-3.2v3.2h3.2V442.3z" />
+          <path d="M57.6,439.1h-3.2v3.2h3.2V439.1z" />
+          <path d="M60.8,435.9h-3.2v3.2h3.2V435.9z" />
+          <path d="M64,432.8h-3.2v3.2H64V432.8z" />
+          <path d="M67.2,429.6H64v3.2h3.2V429.6z" />
+          <path d="M70.4,426.4h-3.2v3.2h3.2V426.4z" />
+          <path d="M73.6,423.2h-3.2v3.2h3.2V423.2z" />
+          <path d="M76.8,420h-3.2v3.2h3.2V420z" />
+          <path d="M80.2,416.6H77v3.2h3.2V416.6z" />
+          <path d="M83.4,413.4h-3.2v3.2h3.2V413.4z" />
+          <path d="M86.6,410.2h-3.2v3.2h3.2V410.2z" />
+          <path d="M89.8,407h-3.2v3.2h3.2V407z" />
+          <path d="M222.3,208.6h-3.2v3.2h3.2V208.6z" />
+          <path d="M225.5,205.4h-3.2v3.2h3.2V205.4z" />
+          <path d="M228.7,202.2h-3.2v3.2h3.2V202.2z" />
+          <path d="M231.9,199h-3.2v3.2h3.2V199z" />
+          <path d="M235.1,195.8h-3.2v3.2h3.2V195.8z" />
+          <path d="M238.2,192.6H235v3.2h3.2V192.6z" />
+          <path d="M241.4,189.4h-3.2v3.2h3.2V189.4z" />
+          <path d="M244.6,186.3h-3.2v3.2h3.2V186.3z" />
+          <path d="M247.8,183.1h-3.2v3.2h3.2V183.1z" />
+          <path d="M251,179.9h-3.2v3.2h3.2V179.9z" />
+          <path d="M254.2,176.7H251v3.2h3.2V176.7z" />
+          <path d="M257.4,173.5h-3.2v3.2h3.2V173.5z" />
+          <path d="M260.6,170.3h-3.2v3.2h3.2V170.3z" />
+          <path d="M263.8,167.1h-3.2v3.2h3.2V167.1z" />
+          <path d="M266.9,163.9h-3.2v3.2h3.2V163.9z" />
+          <path d="M270.1,160.8h-3.2v3.2h3.2V160.8z" />
+          <path d="M273.3,157.6h-3.2v3.2h3.2V157.6z" />
+          <path d="M276.5,154.4h-3.2v3.2h3.2V154.4z" />
+          <path d="M279.7,151.2h-3.2v3.2h3.2V151.2z" />
+          <path d="M282.9,148h-3.2v3.2h3.2V148z" />
+          <path d="M286.3,144.6h-3.2v3.2h3.2V144.6z" />
+          <path d="M289.5,141.4h-3.2v3.2h3.2V141.4z" />
+          <path d="M292.7,138.2h-3.2v3.2h3.2V138.2z" />
+          <path d="M295.9,135h-3.2v3.2h3.2V135z" />
+          <path d="M177.2,253.7H174v3.2h3.2V253.7z" />
+          <path d="M180.4,250.5h-3.2v3.2h3.2V250.5z" />
+          <path d="M183.6,247.3h-3.2v3.2h3.2V247.3z" />
+          <path d="M186.8,244.1h-3.2v3.2h3.2V244.1z" />
+          <path d="M189.9,240.9h-3.2v3.2h3.2V240.9z" />
+          <path d="M193.1,237.8h-3.2v3.2h3.2V237.8z" />
+          <path d="M196.3,234.6h-3.2v3.2h3.2V234.6z" />
+          <path d="M199.5,231.4h-3.2v3.2h3.2V231.4z" />
+          <path d="M202.7,228.2h-3.2v3.2h3.2V228.2z" />
+          <path d="M205.9,225h-3.2v3.2h3.2V225z" />
+          <path d="M209.3,221.6h-3.2v3.2h3.2V221.6z" />
+          <path d="M212.5,218.4h-3.2v3.2h3.2V218.4z" />
+          <path d="M215.7,215.2h-3.2v3.2h3.2V215.2z" />
+          <path d="M218.9,212h-3.2v3.2h3.2V212z" />
+          <path d="M351.6,173.4h-3.2v3.2h3.2V173.4z" />
+          <path d="M354.8,170.2V167h-3.2v3.2v3.2h3.2V170.2z" />
+          <path d="M348.4,176.6H342v3.2h6.4V176.6z" />
+          <path d="M351.6,179.8h-3.2v3.2h3.2V179.8z" />
+          <path d="M354.8,182.9h-3.2v6.4h3.2V182.9z" />
+          <path d="M357.9,179.8h-3.2v3.2h3.2V179.8z" />
+          <path d="M364.3,176.6h-6.4v3.2h6.4V176.6z" />
+          <path d="M357.9,173.4h-3.2v3.2h3.2V173.4z" />
+          <path d="M259.2,69.2H256v3.2h3.2V69.2z" />
+          <path d="M262.4,66h-3.2v3.2h3.2V66z" />
+          <path d="M262.4,72.4h-3.2v3.2h3.2V72.4z" />
+          <path d="M265.6,69.2h-3.2v3.2h3.2V69.2z" />
+          <path d="M100.2,150.2H97v3.2h3.2V150.2z" />
+          <path d="M103.4,147h-3.2v3.2h3.2V147z" />
+          <path d="M103.4,153.4h-3.2v3.2h3.2V153.4z" />
+          <path d="M106.6,150.2h-3.2v3.2h3.2V150.2z" />
+          <path d="M94.2,282.2H91v3.2h3.2V282.2z" />
+          <path d="M97.4,279h-3.2v3.2h3.2V279z" />
+          <path d="M97.4,285.4h-3.2v3.2h3.2V285.4z" />
+          <path d="M100.6,282.2h-3.2v3.2h3.2V282.2z" />
+          <path d="M331.2,549.2H328v3.2h3.2V549.2z" />
+          <path d="M334.4,546h-3.2v3.2h3.2V546z" />
+          <path d="M334.4,552.4h-3.2v3.2h3.2V552.4z" />
+          <path d="M337.6,549.2h-3.2v3.2h3.2V549.2z" />
+          <path d="M3.2,490.2H0v3.2h3.2V490.2z" />
+          <path d="M6.4,487H3.2v3.2h3.2V487z" />
+          <path d="M6.4,493.4H3.2v3.2h3.2V493.4z" />
+          <path d="M9.6,490.2H6.4v3.2h3.2V490.2z" />
+          <path d="M33.6,99.4h-3.2v3.2h3.2V99.4z" />
+          <path d="M36.8,96.2V93h-3.2v3.2v3.2h3.2V96.2z" />
+          <path d="M30.4,102.6H24v3.2h6.4V102.6z" />
+          <path d="M33.6,105.8h-3.2v3.2h3.2V105.8z" />
+          <path d="M36.8,108.9h-3.2v6.4h3.2V108.9z" />
+          <path d="M39.9,105.8h-3.2v3.2h3.2V105.8z" />
+          <path d="M46.3,102.6h-6.4v3.2h6.4V102.6z" />
+          <path d="M39.9,99.4h-3.2v3.2h3.2V99.4z" />
+          <path d="M154.6,604.4h-3.2v3.2h3.2V604.4z" />
+          <path d="M157.8,601.2V598h-3.2v3.2v3.2h3.2V601.2z" />
+          <path d="M151.4,607.6H145v3.2h6.4V607.6z" />
+          <path d="M154.6,610.8h-3.2v3.2h3.2V610.8z" />
+          <path d="M157.8,613.9h-3.2v6.4h3.2V613.9z" />
+          <path d="M160.9,610.8h-3.2v3.2h3.2V610.8z" />
+          <path d="M167.3,607.6h-6.4v3.2h6.4V607.6z" />
+          <path d="M160.9,604.4h-3.2v3.2h3.2V604.4z" />
+        </g>
+        <g className="fill-white">
+          <path d="M210.5,444h-24.8v24.8h24.8V444z" />
+          <path d="M235.3,419.1h-24.8V444h24.8V419.1z" />
+          <path d="M285,394.3h-24.8v24.8H285V394.3z" />
+          <path d="M260.2,294.9h-24.8v99.4h24.8V294.9z" />
+          <path d="M185.7,419.1h-24.8V444h24.8V419.1z" />
+          <path d="M160.8,294.9H136v124.2h24.8V294.9z" />
+        </g>
+      </svg>
+    </div>
+  );
+};
+
+export default PlanetRocket;
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ import ButtonLink from "../components/ButtonLink";
 import PageSection from "../components/PageSection";
 import Calendar from "../components/Calendar";
 import Planet from "../components/Svg/Planet";
+import PlanetRocket from "../components/Svg/PlanetRocket";
 
 const IndexPage = () => {
   const intl = useIntl();
@@ -50,25 +51,31 @@ const IndexPage = () => {
 
         <Container>
           <PageSection>
-            <div className="md:grid md:grid-cols-12">
-              <div className="md:col-span-8">
-                <div className="title-l mb-4">
-                  We're throwing open the doors to the new financial system
-                  &mdash; and a truly democratic society.
-                </div>
-                <div className="copy-s !mb-8">
-                  Don’t ask for permission. Ask how you can play a part in this
-                  new world of DeFi. Decentralised derivatives markets are here
-                  to stay.
-                </div>
-                <div>
-                  <div className="inline-block mb-4 mr-4">
-                    <ButtonLink text="Learn more" link="/key-concepts" />
+            <div className="md:grid md:grid-cols-12 items-center">
+              <div className="md:col-span-7 pr-12">
+                <div className="max-w-[38.75rem]">
+                  <div className="title-l mb-4">
+                    We're throwing open the doors to the new financial system
+                    &mdash; and a truly democratic society.
                   </div>
-                  <ButtonLink text="Join the community" link="/community" />
+                  <div className="copy-s !mb-8">
+                    Don’t ask for permission. Ask how you can play a part in
+                    this new world of DeFi. Decentralised derivatives markets
+                    are here to stay.
+                  </div>
+                  <div>
+                    <div className="inline-block mb-4 mr-4">
+                      <ButtonLink text="Learn more" link="/key-concepts" />
+                    </div>
+                    <ButtonLink text="Join the community" link="/community" />
+                  </div>
                 </div>
               </div>
-              <div className="md:col-span-4"></div>
+              <div className="md:col-span-5 mt-20 md:mt-0">
+                <div className="max-w-[32rem] mx-auto">
+                  <PlanetRocket />
+                </div>
+              </div>
             </div>
           </PageSection>
 
