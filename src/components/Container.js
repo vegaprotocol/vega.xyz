ce7c2da4c9cec2320540af08ac61c4facb9a4a27
Oliver Gosling <oli@big.fan>
2022-01-20 14:33:21 +0000

    fix container

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Container = (props) => {
-  return (
-    <div className="mx-auto px-4 md:px-6 lg:px-8 2xl:pt-8">
-      {props.children}
-    </div>
-  );
+  return <div className="mx-auto px-4 md:px-6 lg:px-8">{props.children}</div>;
 };
 
 export default Container;
