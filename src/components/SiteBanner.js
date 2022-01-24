441ac2c725b67f2bcea45e91616e0894921131e6
Oliver Gosling <oli@big.fan>
2022-01-24 13:40:22 +0000

    update site banner

--- src/components/SiteBanner.js
+++ src/components/SiteBanner.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "gatsby";
 import styled from "styled-components";
 import siteBannerBackground from "../images/site-banner-background.png";
 
@@ -6,31 +7,14 @@ const Banner = styled.div`
   animation-name: panBackground;
   background-image: url(${siteBannerBackground});
   background-size: cover;
-
-  // animation-name: panBackground;
-  // animation-duration: 100s;
-  // animation-fill-mode: both;
-  // animation-iteration-count: infinite;
-
-  // @keyframes panBackground {
-  //   0% {
-  //     background-position: 0 0;
-  //   }
-  //   50% {
-  //     background-position: 100% 100%;
-  //   }
-  //   100% {
-  //     background-position: 0% 0%;
-  //   }
-  // }
 `;
 
 const SiteBanner = () => {
   return (
     <div>
       <Banner>
-        <div className="container mx-auto py-3 text-center text-white">
-          WATCH OUR KEYNOTE FROM ETH DENVER
+        <div className="container mx-auto py-3 text-center text-white underline hover:no-underline">
+          <Link to="/ethdenver">Vega does ETHDenver 2022</Link>
         </div>
       </Banner>
     </div>
