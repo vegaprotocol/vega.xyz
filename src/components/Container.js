21abd8e41f8c61b34a45e60dffc032bbd6f89f81
Oliver Gosling <oli@big.fan>
2022-01-24 17:07:03 +0000

    update container component

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Container = (props) => {
   return (
-    <div className="mx-auto px-4 md:px-6 lg:px-8 max-w-[90rem] mx-auto">
-      {props.children}
+    <div className={props.hideXOverflow ? "overflow-x-hidden" : ""}>
+      <div className="mx-auto px-4 md:px-6 lg:px-8 max-w-[90rem]">
+        {props.children}
+      </div>
     </div>
   );
 };
