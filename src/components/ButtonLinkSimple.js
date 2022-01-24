ac53c11e87868a4239213feebcd908081bb7d264
Oliver Gosling <oli@big.fan>
2022-01-24 11:35:19 +0000

    update button to work with onclick

--- src/components/ButtonLinkSimple.js
+++ src/components/ButtonLinkSimple.js
@@ -2,27 +2,38 @@ import { Link } from "gatsby";
 import React from "react";
 import LinkArrow from "./Svg/LinkArrow";
 
-const ButtonLinkSimple = ({ text, link }) => {
-  const isExternal = link.startsWith("http");
-  const buttonClass = `inline-block px-4 py-3 leading-1 text-[0.9375rem] tracking-[0.01rem] border border-current text-current uppercase`;
+const ButtonLinkSimple = ({ text, link, onClick }) => {
+  const buttonClass = `inline-block px-8 py-3 leading-1 text-[0.9375rem] tracking-[0.01rem] border border-current text-current uppercase`;
 
-  if (isExternal) {
+  if (onClick) {
     return (
-      <a href={link} target="_blank" rel="noreferrer" className={buttonClass}>
+      <button className={buttonClass} onClick={onClick}>
         {text}
-        {link.startsWith("http") && (
-          <span className="inline-block ml-2">
-            <LinkArrow />
-          </span>
-        )}
-      </a>
+        <span className="inline-block ml-2">
+          <LinkArrow />
+        </span>
+      </button>
     );
   } else {
-    return (
-      <Link to={link} className={buttonClass}>
-        {text}
-      </Link>
-    );
+    const isExternal = link.startsWith("http");
+    if (isExternal) {
+      return (
+        <a href={link} target="_blank" rel="noreferrer" className={buttonClass}>
+          {text}
+          {link.startsWith("http") && (
+            <span className="inline-block ml-2">
+              <LinkArrow />
+            </span>
+          )}
+        </a>
+      );
+    } else {
+      return (
+        <Link to={link} className={buttonClass}>
+          {text}
+        </Link>
+      );
+    }
   }
 };
 
