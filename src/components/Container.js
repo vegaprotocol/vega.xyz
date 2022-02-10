0c75912c1f82bdb8d999428818d427e370c3fee4
David Boydell <david.boydell@bbc.co.uk>
2022-02-10 15:56:40 +0000

    Conditional check for data-cy on container component

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const Container = (props) => {
   return (
-    <div className={props.hideXOverflow ? "overflow-x-hidden" : ""}>
+    <div className={props.hideXOverflow ? "overflow-x-hidden" : ""}
+      {...(props.dataCy ? {'data-cy': 'main'} : {})}>
       <div className="mx-auto px-4 md:px-6 lg:px-8 max-w-[90rem]">
         {props.children}
       </div>
