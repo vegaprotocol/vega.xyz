9483befaeca00ff2d43a45c3f68239624f57b4eb
Oliver Gosling <oli@big.fan>
2022-03-01 12:48:22 +0000

    fix first ticker cell to left

--- src/components/TickerCell.js
+++ src/components/TickerCell.js
@@ -4,7 +4,7 @@ import TextLink from "./TextLink";
 
 const TickerCell = ({ title, value, text, link }) => {
   return (
-    <div className="px-6 py-4 pr-16 border-l border-r-0 border-current min-w-[12.5rem] flex items-center">
+    <div className="px-6 py-2 md:py-4 pr-16 border-l border-r-0 border-current min-w-[12.5rem] flex items-center">
       <div>
         {title && (
           <div className="uppercase text-[0.9375rem]">
@@ -13,11 +13,13 @@ const TickerCell = ({ title, value, text, link }) => {
         )}
 
         {value && (
-          <div className="mt-1 text-[1.875rem] leading-none">{value}</div>
+          <div className="mt-1 text-[1.5rem] lg:text-[1.875rem] leading-none">
+            {value}
+          </div>
         )}
 
         {text && (
-          <div className="text-[1.875rem] leading-none uppercase">
+          <div className="text-[1.5rem] lg:text-[1.875rem] leading-none uppercase">
             {link ? <TextLink to={link}>{text}</TextLink> : <div>text</div>}
           </div>
         )}
