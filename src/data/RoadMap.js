465c19f1198b8431591a36e42fe0cfed3c13fda2
Oliver Gosling <oli@big.fan>
2022-05-31 08:55:26 +0100

    update roadmap dates

--- src/data/RoadMap.js
+++ src/data/RoadMap.js
@@ -6,19 +6,19 @@ export const RoadMapContent = [
       "This is the second of 3 launch phases of the network in which the validators are running a decentralised Vega network connected to the Ethereum mainnet. This phase provides a proving ground for the proof of stake network, allows time for the community to learn how to reliably operate nodes, and introduces on-chain governance. Token holders can participate in governance, and stake and delegate their tokens.",
   },
   {
-    title1: "H1 2022",
+    title1: "H2 2022",
     title2: "Alpha<br />Mainnet",
     content:
       "In this phase the project contributors and network validators will signal that all required tests and network upgrades have completed succesfully and the community will vote to allow the creation of markets and trading for the first time in mainnet. During this final launch phase, the network is expected to operate with fairly conservative community controlled limits on the amount of funds that may be risked. Early adopters, community, and ecosystem members will be able to trade for the first time, and use this phase to try out their ideas for markets and test integrations with their own projects and protocols.",
   },
   {
-    title1: "H2 2022",
+    title1: "H1 2023",
     title2: "Version 1<br />Mainnet",
     content:
       "Full Mainnet begins with successful completion of the final launch phase. This begins when the community have signalled their confidence in the software via on-chain governance votes and all remaining temporary limits have been removed. From this point, the development of the protocol and software will focus on enabling market innovation and iterating on feedback received from the community. This will include the addition of key features not present in the initial MVP, examples of which could include spot markets, passive liquidity, and Ethereum oracles. New bridges to other blockchains and/or L2s may also be introduced during this phase.",
   },
   {
-    title1: "H1 2023",
+    title1: "H2 2023",
     title2: "Version 2<br />Mainnet",
     content:
       "This release will allow the validators to launch an experimental mainnet using the second major release of the Vega protocol. The V2 protocol will use WASM to allow the community to create their own products & risk models, and control other aspects of the protocol. This version will also contain a number of performance and security improvements as well as core protocol upgrades and simplifications.",
