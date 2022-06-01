cf334006f67071896e2d6a91584a8bb35bf2a32a
Oliver Gosling <oli@big.fan>
2022-06-01 15:01:15 +0100

    tidy up text link component

--- src/components/TextLink.js
+++ src/components/TextLink.js
@@ -1,35 +1,3 @@
-// import { Link } from "gatsby";
-// import React from "react";
-// import LinkArrow from "./Svg/LinkArrow";
-
-// const TextLink = (props) => {
-//   const isExternal = props.to.startsWith("http");
-//   const classes = `inline-block copy-xxs md:copy-xs text-vega-mid-grey dark:text-vega-grey underline hover:no-underline ${props.className}`;
-//   if (isExternal) {
-//     return (
-//       <a href={props.to} target="_blank" rel="noreferrer" className={classes}>
-//         {props.children}
-//         {props.to.startsWith("http") && (
-//           <span className="inline-block ml-2 align-middle">
-//             <LinkArrow className="text-white" />
-//           </span>
-//         )}
-//       </a>
-//     );
-//   } else {
-//     return (
-//       <Link to={props.to} className={classes}>
-//         {props.children}
-//       </Link>
-//     );
-//   }
-// };
-
-// export default TextLink;
-
-// colour white or grey
-// unerline or not
-
 import { Link } from "gatsby";
 import React from "react";
 import LinkArrow from "./Svg/LinkArrow";
