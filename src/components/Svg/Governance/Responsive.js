48153e244e9c52a38223a718c5db769b244deae1
Oliver Gosling <oli@big.fan>
2022-06-01 15:00:32 +0100

    add governance hero image

---
+++ src/components/Svg/Governance/Responsive.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Small from "./Small";
+import Medium from "./Medium";
+import Large from "./Large";
+import XLarge from "./XLarge";
+import {
+  MQXLarge,
+  MQLarge,
+  MQMedium,
+  MQSmall,
+} from "../../../utils/media-queries.js";
+
+const GovernanceResponsive = () => {
+  return (
+    <div>
+      <MQSmall>
+        <Small />
+      </MQSmall>
+      <MQMedium>
+        <Medium />
+      </MQMedium>
+      <MQLarge>
+        <Large />
+      </MQLarge>
+      <MQXLarge>
+        <div className="max-w-[100rem] mx-auto">
+          <XLarge />
+        </div>
+      </MQXLarge>
+    </div>
+  );
+};
+export default GovernanceResponsive;
