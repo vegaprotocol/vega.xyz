c924ecf97ce837578950b6cf25317999bf6ddddf
Oliver Gosling <oli@big.fan>
2022-07-12 15:23:15 +0100

    remove comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -106,17 +106,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-react-i18next`,
       options: {
-        localeJsonSourceName: `locale`, // name given to `gatsby-source-filesystem` plugin.
+        localeJsonSourceName: `locale`,
         languages: [`en`, `es`],
         defaultLanguage: `en`,
-        // if you are using Helmet, you must include siteUrl, and make sure you add http:https
         siteUrl: `https://vega.xyz`,
-        // if you are using trailingSlash gatsby config include it here, as well (the default is 'always')
         trailingSlash: "always",
-        // you can pass any i18next options
         i18nextOptions: {
           interpolation: {
-            escapeValue: false, // not needed for react as it escapes by default
+            escapeValue: false,
           },
           keySeparator: false,
           nsSeparator: false,
