938b13db19bd48d04de31adf8ac948f6a0ccb363
Oliver Gosling <oli@big.fan>
2022-08-12 11:17:51 +0100

    fix language toggle

--- src/components/LanguageToggle.js
+++ src/components/LanguageToggle.js
@@ -79,7 +79,8 @@ const LanguageToggle = () => {
             <li key={lng}>
               <button
                 href="#"
-                className="min-w-[8.125rem] inline-block px-3 py-1.5 rounded-[2px] hover:dark:bg-vega-lightest-grey hover:bg-white"
+                tabindex={0}
+                className="text-left min-w-[8.125rem] inline-block px-3 py-1.5 rounded-[2px] hover:dark:bg-vega-lightest-grey hover:bg-white"
                 onClick={(e) => {
                   e.preventDefault();
                   switchLanguage(lng);
