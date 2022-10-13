3d1afdbe994253afc24ade4178255d29305e7786
Oliver Gosling <oli@big.fan>
2022-10-13 11:10:29 +0100

    add network params component

---
+++ src/components/NetworkParameter.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { useNetworkParams } from "../hooks/use-network-params";
+import { SnakeToCamel } from "../utils/tools";
+import { Trans, useTranslation } from "gatsby-plugin-react-i18next";
+import ParameterBox from "./ParameterBox";
+
+export interface NetworkParameterProps {
+  param: string;
+  prefix?: string;
+  suffix?: string;
+  formatForVega?: boolean;
+  expressPercentage?: boolean;
+}
+
+const explorerUrl = "https://explorer.vega.xyz/network-parameters";
+
+const NetworkParameter = ({
+  param,
+  prefix,
+  suffix,
+  formatForVega = false,
+  expressPercentage = false,
+}: NetworkParameterProps) => {
+  const { params, loading, error } = useNetworkParams();
+  const { t } = useTranslation("component.network-parameter");
+
+  const formatVegaValue = (value) => {
+    return (value / 1000000000000000000).toFixed(2);
+  };
+
+  const formatValue = (value) => {
+    if (formatForVega) {
+      return formatVegaValue(value);
+    } else if (expressPercentage) {
+      return value * 100;
+    } else {
+      return value;
+    }
+  };
+
+  return (
+    <>
+      {loading && (
+        <span>
+          <Trans t={t}>Loading...</Trans>
+        </span>
+      )}
+      {error && (
+        <span>
+          <Trans t={t}>Error loading value</Trans>
+        </span>
+      )}
+      {params && (
+        <a href={`${explorerUrl}#${SnakeToCamel(param)}`} target="_blank">
+          <ParameterBox
+            value={formatValue(params[param])}
+            prefix={prefix}
+            suffix={suffix}
+            description={`This value is determined by the network parameter ‘${SnakeToCamel(
+              param
+            )}’`}
+          />
+        </a>
+      )}
+    </>
+  );
+};
+
+export default NetworkParameter;
---
+++ src/components/ParameterBox.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Tooltip from "./UI/Tooltip";
+import Tippy from "@tippyjs/react";
+
+export interface NetworkParameterProps {
+  value: string;
+  description?: string;
+  prefix?: string;
+  suffix?: string;
+}
+
+const ParameterBox = ({
+  value,
+  prefix,
+  suffix,
+  description,
+}: NetworkParameterProps) => {
+  return (
+    <>
+      <Tippy
+        content={
+          <div className="dark:text-white text-black mt-2 body-s text-left font-not-glitched">
+            <Tooltip>{description}</Tooltip>
+          </div>
+        }
+      >
+        <span className="group cursor-pointer px-1.5 inline-block relative dark:text-white text-black dark:bg-[#404040] dark:hover:bg-[#8B8B8B] bg-[#D2D2D2] hover:bg-[#A7A7A7]">
+          {prefix && `${prefix}`}
+          {value}
+          {suffix && `${suffix}`}
+
+          {description && (
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="inline-block ml-1.5 align-middle relative -top-px"
+            >
+              <rect
+                x="1"
+                y="15"
+                width="14"
+                height="14"
+                rx="7"
+                transform="rotate(-90 1 15)"
+                className="dark:fill-[#8B8B8B] dark:group-hover:fill-[#C0C0C0] fill-[#A7A7A7] group-hover:fill-[#626262]"
+              />
+              <rect x="7.5" y="4.5" width="1" height="1" fill="white" />
+              <rect x="7.5" y="6.5" width="1" height="5" fill="white" />
+            </svg>
+          )}
+        </span>
+      </Tippy>
+    </>
+  );
+};
+
+export default ParameterBox;
