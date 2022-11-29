f02b66efabcb517ef1325bcbe0cf3c2ffa4b62fd
Oliver Gosling <goslingo@gmail.com>
2022-11-29 14:10:26 +0000

    Add Insights to header and footer navigation

--- src/components/Navigation/items/Learn.js
+++ src/components/Navigation/items/Learn.js
@@ -1,29 +1,30 @@
-import React from "react";
-import NavigationItem from "../Item";
-import NavigationList from "../List";
-import NavigationHeading from "../Heading";
-import { Trans, useTranslation } from "gatsby-plugin-react-i18next";
+import React from 'react'
+import NavigationItem from '../Item'
+import NavigationList from '../List'
+import NavigationHeading from '../Heading'
+import { Trans, useTranslation } from 'gatsby-plugin-react-i18next'
 
 const Learn = () => {
-  const { t } = useTranslation("component.navigation");
+  const { t } = useTranslation('component.navigation')
   return (
-    <div className="lg:grid lg:grid-cols-3 gap-6">
+    <div className="gap-6 lg:grid lg:grid-cols-3">
       <div>
         <NavigationHeading>
           <Trans t={t}>Explore</Trans>
         </NavigationHeading>
         <NavigationList>
-          <NavigationItem text={t("Key concepts")} link="/key-concepts" />
-          <NavigationItem text={t("Vega papers")} link="/papers" />
+          <NavigationItem text={t('Key concepts')} link="/key-concepts" />
+          <NavigationItem text={t('Vega papers')} link="/papers" />
           <NavigationItem
-            text={t("Whitepaper")}
+            text={t('Whitepaper')}
             link="https://vega.xyz/papers/vega-protocol-whitepaper.pdf"
           />
           <NavigationItem
-            text={t("Technical overview")}
+            text={t('Technical overview')}
             link="https://vega.xyz/papers/vega-technical-overview.pdf"
           />
-          <NavigationItem text={t("Talks")} link="/talks" />
+          <NavigationItem text={t('Insights')} link="/insights" />
+          <NavigationItem text={t('Talks')} link="/talks" />
         </NavigationList>
       </div>
       <div>
@@ -31,11 +32,11 @@ const Learn = () => {
           <Trans t={t}>Latest</Trans>
         </NavigationHeading>
         <NavigationList>
-          <NavigationItem text={t("Blog")} link="https://blog.vega.xyz" />
+          <NavigationItem text={t('Blog')} link="https://blog.vega.xyz" />
         </NavigationList>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Learn;
+export default Learn
