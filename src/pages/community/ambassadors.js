02413f63e9bb9da002ed3fba617fb377a534c55e
Oliver Gosling <goslingo@gmail.com>
2022-12-02 11:16:50 +0000

    Fixes missing translations on ambassadors page

--- src/pages/community/ambassadors.js
+++ src/pages/community/ambassadors.js
@@ -1,40 +1,41 @@
-import React, { useState } from "react";
-import { graphql } from "gatsby";
-import Seo from "../../components/Seo";
-import Layout from "../../components/Layout";
-import TranslationsBanner from "../../components/TranslationsBanner";
-import Container from "../../components/Container";
-import GlitchTitle from "../../components/GlitchTitle";
-import LeadingLine from "../../components/LeadingLine";
-import BoxLinkSimple from "../../components/BoxLinkSimple";
-import ButtonLink from "../../components/ButtonLink";
-import AmbassadorsHero from "../../components/Svg/AmbassadorsHero";
-import IconAmbassador1 from "../../images/icon-ambassador-1.png";
-import IconAmbassador2 from "../../images/icon-ambassador-2.png";
-import { Trans, useTranslation } from "gatsby-plugin-react-i18next";
-import AmbassadorLeaderboard from "../../components/AmbassadorLeaderboard";
+import React, { useState } from 'react'
+import { graphql } from 'gatsby'
+import Seo from '../../components/Seo'
+import Layout from '../../components/Layout'
+import TranslationsBanner from '../../components/TranslationsBanner'
+import Container from '../../components/Container'
+import GlitchTitle from '../../components/GlitchTitle'
+import LeadingLine from '../../components/LeadingLine'
+import BoxLinkSimple from '../../components/BoxLinkSimple'
+import ButtonLink from '../../components/ButtonLink'
+import AmbassadorsHero from '../../components/Svg/AmbassadorsHero'
+import IconAmbassador1 from '../../images/icon-ambassador-1.png'
+import IconAmbassador2 from '../../images/icon-ambassador-2.png'
+import IconAmbassador3 from '../../images/icon-ambassador-3.png'
+import { Trans, useTranslation } from 'gatsby-plugin-react-i18next'
+import AmbassadorLeaderboard from '../../components/AmbassadorLeaderboard'
 
 const Clan = ({ icon, title, text }) => {
   return (
-    <div className="flex items-center gap-6 mb-6">
+    <div className="mb-6 flex items-center gap-6">
       <img src={icon} width="93" height="93" alt="" className="shrink-0" />
       <div>
         <h3 className="title-s md:title-m mb-2">{title}</h3>
         <p className="copy-xxs md:copy-xs !mb-0">{text}</p>
       </div>
     </div>
-  );
-};
+  )
+}
 
 const benefits = [
-  "Gain real-world experience in community building by shaping the Vega movement from the ground up",
-  "Access to exclusive Discord lounges",
-  "Early access to bounties, beta testing of products",
-  "Priority consideration on protocol feedback",
+  'Gain real-world experience in community building by shaping the Vega movement from the ground up',
+  'Access to exclusive Discord lounges',
+  'Early access to bounties, beta testing of products',
+  'Priority consideration on protocol feedback',
   "Exclusive meetups - Invitations to all of Vega's events, both offline and online",
-  "Limited edition Vega swag",
-  "Grow with Vega - scaling rewards as you climb the ranks",
-];
+  'Limited edition Vega swag',
+  'Grow with Vega - scaling rewards as you climb the ranks',
+]
 
 // t("Gain real-world experience in community building by shaping the Vega movement from the ground up"),
 // t("Access to exclusive Discord lounges"),
@@ -45,31 +46,31 @@ const benefits = [
 // t("Grow with Vega - scaling rewards as you climb the ranks"),
 
 const Ambassadors = () => {
-  const { i18n, t } = useTranslation("page.community.ambassadors");
-  const [missingTranslations, setMissingTranslations] = useState(false);
+  const { i18n, t } = useTranslation('page.community.ambassadors')
+  const [missingTranslations, setMissingTranslations] = useState(false)
 
-  i18n.on("missingKey", (lng) => {
-    setMissingTranslations(true);
-  });
+  i18n.on('missingKey', (lng) => {
+    setMissingTranslations(true)
+  })
 
   return (
     <Layout>
       <Seo
-        title={t("Be a Vega+ Ambassador")}
+        title={t('Be a Vega+ Ambassador')}
         description={t(
-          "Knowledgeable in cryptocurrencies, DeFi and DAOs? We want you to be a driving force of the Vega community."
+          'Knowledgeable in cryptocurrencies, DeFi and DAOs? We want you to be a driving force of the Vega community.'
         )}
       />
       {missingTranslations && <TranslationsBanner />}
       <Container>
-        <div className="max-w-[21.25rem] md:max-w-[40rem] lg:max-w-[80rem] mx-auto text-center md:mb-12 pt-6 lg:pt-16">
+        <div className="mx-auto max-w-[21.25rem] pt-6 text-center md:mb-12 md:max-w-[40rem] lg:max-w-[80rem] lg:pt-16">
           <GlitchTitle
             level={1}
-            className="mb-4 title-m md:title-xxl lg:title-xxxl md:mb-6"
+            className="title-m md:title-xxl lg:title-xxxl mb-4 md:mb-6"
           >
             <Trans t={t}>Be a Vega+ Ambassador</Trans>
           </GlitchTitle>
-          <LeadingLine className="text-center text-current mx-auto max-w-[35rem]">
+          <LeadingLine className="mx-auto max-w-[35rem] text-center text-current">
             <Trans t={t}>
               Seeking emissaries of the future to co-create the leading crypto
               derivatives layer.
@@ -79,12 +80,12 @@ const Ambassadors = () => {
       </Container>
 
       <Container hideXOverflow={true}>
-        <div className="max-w-[55.5rem] mx-auto mb-6 md:mb-16">
-          <AmbassadorsHero className="scale-125 my-12 md:my-0 md:scale-100" />
+        <div className="mx-auto mb-6 max-w-[55.5rem] md:mb-16">
+          <AmbassadorsHero className="my-12 scale-125 md:my-0 md:scale-100" />
         </div>
       </Container>
 
-      <Container dataCy={"main"}>
+      <Container dataCy={'main'}>
         <div className="md:grid md:grid-cols-12 md:gap-x-12 lg:gap-x-6">
           <div className="md:col-span-8 lg:col-span-7">
             <div className="mb-12 md:mb-20">
@@ -115,8 +116,8 @@ const Ambassadors = () => {
 
               <ButtonLink
                 link="https://vegaprotocol.typeform.com/ambassadorappq4"
-                text={t("Apply now")}
-                className="md:hidden mt-3"
+                text={t('Apply now')}
+                className="mt-3 md:hidden"
               />
             </div>
 
@@ -126,23 +127,21 @@ const Ambassadors = () => {
               </h2>
               <div className="mt-6 md:mt-12">
                 <Clan
-                  title={t("Builders Club (Technical)")}
+                  title={t('Builders Club (Technical)')}
                   text={t(
-                    "(Vega Capsule, Documentation, Hackathon Support, Coding)"
+                    '(Vega Capsule, Documentation, Hackathon Support, Coding)'
                   )}
                   icon={IconAmbassador2}
                 />
-                  <Clan
-                  title={t("Content Collective (Non-Technical)")}
-                  text={t(
-                    "(Tweets, Editorials, Infographics, Bounties)"
-                  )}
+                <Clan
+                  title={t('Content Collective (Non-Technical)')}
+                  text={t('(Tweets, Editorials, Infographics, Bounties)')}
                   icon={IconAmbassador1}
                 />
                 <Clan
-                  title={t("Multilingual League (Translations)")}
+                  title={t('Multilingual League (Translations)')}
                   text={t(
-                         "(Internationalisation, Local Language Groups, AMAs)"
+                    '(Internationalisation, Local Language Groups, AMAs)'
                   )}
                   icon={IconAmbassador3}
                 />
@@ -166,28 +165,28 @@ const Ambassadors = () => {
                 </Trans>
               </p>
 
-              <div className="border-t border-current mt-8 md:mt-12 mb-4 md:mb-0">
+              <div className="mt-8 mb-4 border-t border-current md:mt-12 md:mb-0">
                 {benefits.map((benefit, idx) => {
                   return (
                     <div
                       key={idx}
-                      className="flex text-current items-center border-b border-current py-3 md:py-6"
+                      className="flex items-center border-b border-current py-3 text-current md:py-6"
                     >
-                      <div className="copy-m !mb-0 md:copy-l shrink-0 min-w-[3rem] md:min-w-[5rem]">
+                      <div className="copy-m md:copy-l !mb-0 min-w-[3rem] shrink-0 md:min-w-[5rem]">
                         {idx + 1}
                       </div>
-                      <div className="copy-xs md:copy-s text-current !mb-0">
+                      <div className="copy-xs md:copy-s !mb-0 text-current">
                         {t(benefit)}
                       </div>
                     </div>
-                  );
+                  )
                 })}
               </div>
 
               <ButtonLink
                 link="https://vegaprotocol.typeform.com/ambassadorappq4"
                 text="Apply now"
-                className="md:hidden mt-3"
+                className="mt-3 md:hidden"
               />
             </div>
 
@@ -211,7 +210,7 @@ const Ambassadors = () => {
               <h2 className="title-s md:title-l mb-4">
                 <Trans t={t}>Expectations</Trans>
               </h2>
-              <p className="copy-xs md:copy-s text-current mb-8">
+              <p className="copy-xs md:copy-s mb-8 text-current">
                 <Trans t={t}>
                   There are no exact requirements to being an ambassador; the
                   extent of what you'll work on will depend on your team and
@@ -219,7 +218,7 @@ const Ambassadors = () => {
                 </Trans>
               </p>
               <div className="prose-xl dark:text-white">
-                <ul className="list-disc copy-xs md:copy-s mt-8 md:mt-12">
+                <ul className="copy-xs md:copy-s mt-8 list-disc md:mt-12">
                   <li>
                     <Trans t={t}>
                       Agree with the code of conduct; keep to the rules
@@ -254,9 +253,9 @@ const Ambassadors = () => {
             </div>
           </div>
 
-          <div className="hidden lg:block lg:col-span-1"></div>
+          <div className="hidden lg:col-span-1 lg:block"></div>
 
-          <div className="hidden md:block md:col-span-4 lg:col-span-4 mb-20">
+          <div className="mb-20 hidden md:col-span-4 md:block lg:col-span-4">
             <div className="sticky top-[6.75rem]">
               <div className="title-s mb-3">
                 <Trans t={t}>Interested in joining?</Trans>
@@ -269,8 +268,8 @@ const Ambassadors = () => {
               </p>
               <BoxLinkSimple
                 link="https://vegaprotocol.typeform.com/ambassadorappq4"
-                text={t("Apply now")}
-                className="mt-4 min-w-[15rem] text-left hidden md:inline-block"
+                text={t('Apply now')}
+                className="mt-4 hidden min-w-[15rem] text-left md:inline-block"
               />
             </div>
           </div>
@@ -281,58 +280,58 @@ const Ambassadors = () => {
             <GlitchTitle
               color="red"
               level="2"
-              className="mb-4 title-m md:title-l lg:title-xxl md:max-w-[35rem] mx-auto"
+              className="title-m md:title-l lg:title-xxl mx-auto mb-4 md:max-w-[35rem]"
             >
               <Trans t={t}>Join the community</Trans>
             </GlitchTitle>
           </div>
 
-          <div className="max-w-[50rem] grid gap-4 grid-cols-2 mt-12 mx-auto md:grid-cols-3">
+          <div className="mx-auto mt-12 grid max-w-[50rem] grid-cols-2 gap-4 md:grid-cols-3">
             <BoxLinkSimple
-              text={t("Discord")}
+              text={t('Discord')}
               link="https://vega.xyz/discord"
             />
             <BoxLinkSimple
-              text={t("Telegram")}
+              text={t('Telegram')}
               link="https://t.me/vegacommunity/"
             />
 
             <BoxLinkSimple
-              text={t("Github")}
+              text={t('Github')}
               link="https://github.com/vegaprotocol/"
             />
             <BoxLinkSimple
-              text={t("Mainnet Docs")}
+              text={t('Mainnet Docs')}
               link="https://docs.vega.xyz/"
             />
             <BoxLinkSimple
-              text={t("Fairground Docs")}
+              text={t('Fairground Docs')}
               link="https://docs.fairground.vega.xyz/"
             />
             <BoxLinkSimple
-              text={t("Twitter")}
+              text={t('Twitter')}
               link="https://twitter.com/vegaprotocol"
             />
             <BoxLinkSimple
-              text={t("Forum")}
+              text={t('Forum')}
               link="https://community.vega.xyz/"
             />
             <BoxLinkSimple
-              text={t("YouTube")}
+              text={t('YouTube')}
               link="https://www.youtube.com/vegaprotocol/"
             />
             <BoxLinkSimple
-              text={t("Twitch")}
+              text={t('Twitch')}
               link="https://www.twitch.tv/vegaprotocol/"
             />
           </div>
         </div>
       </Container>
     </Layout>
-  );
-};
+  )
+}
 
-export default Ambassadors;
+export default Ambassadors
 
 export const query = graphql`
   query ($language: String!) {
@@ -346,4 +345,4 @@ export const query = graphql`
       }
     }
   }
-`;
+`
