a51124380bba93d004eea1e1185bb40716c318ff
Oliver Gosling <goslingo@gmail.com>
2022-12-08 12:41:32 +0000

    Display 404 page as a component rather than redirecting

--- src/pages/external-link/index.tsx
+++ src/pages/external-link/index.tsx
@@ -1,19 +1,18 @@
 import React, { useEffect, useState } from 'react'
-//import { navigate } from '@reach/router'
 import { graphql } from 'gatsby'
 import Container from '../../components/Container'
 import GlitchTitle from '../../components/UI/GlitchTitle'
 import Seo from '../../components/Seo'
 import { Trans, useTranslation } from 'gatsby-plugin-react-i18next'
 import interstitialAllowList from '../../../interstitial-allow.json'
-import { createBrowserHistory } from 'history'
+import NotFoundPage from '../404'
 
 const ExternalLinkPage = () => {
   const { t, i18n } = useTranslation('page.external-link')
   const [missingTranslations, setMissingTranslations] = useState(false)
   const [seconds, setSeconds] = useState(5)
+  const [notFound, setNotFound] = useState(false)
   const [url, setUrl] = useState('')
-  const history = createBrowserHistory()
 
   i18n.on('missingKey', (lng) => {
     setMissingTranslations(true)
@@ -34,7 +33,7 @@ const ExternalLinkPage = () => {
         .map((url) => url.replace(/\/$/, ''))
         .includes(urlValue.replace(/\/$/, '')) // strip trailing slashes for comparison
     ) {
-      history.replace('/404')
+      setNotFound(true)
     }
 
     const timer = setInterval(() => {
@@ -52,35 +51,41 @@ const ExternalLinkPage = () => {
   }, [seconds])
 
   return (
-    <div className="flex min-h-screen items-center justify-center text-black dark:text-white">
-      <Seo title={t("You're leaving Vega.xyz")} />
-      <Container dataCy={'main'}>
-        <div className="pt-space-4 pb-space-8 text-center md:pt-space-14 md:pb-space-14">
-          <h1 className="heading-xl mx-auto mb-space-6 max-w-[45rem]">
-            <GlitchTitle>
-              <Trans t={t}>You're leaving Vega.xyz</Trans>
-            </GlitchTitle>
-          </h1>
-          <p className="body-xl mx-auto mb-space-4 max-w-[40rem]">
-            <Trans t={t}>
-              The link you are following is not hosted by Vega. You will be
-              redirected in
-            </Trans>{' '}
-            {seconds}...
-          </p>
-          {url && (
-            <p className="body-xl">
-              <a
-                href={url}
-                className="underline hover:no-underline"
-                rel="noreferrer"
-              >
-                {url}
-              </a>
-            </p>
-          )}
+    <div>
+      {notFound ? (
+        <NotFoundPage />
+      ) : (
+        <div className="flex min-h-screen items-center justify-center text-black dark:text-white">
+          <Seo title={t("You're leaving Vega.xyz")} />
+          <Container dataCy={'main'}>
+            <div className="pt-space-4 pb-space-8 text-center md:pt-space-14 md:pb-space-14">
+              <h1 className="heading-xl mx-auto mb-space-6 max-w-[45rem]">
+                <GlitchTitle>
+                  <Trans t={t}>You're leaving Vega.xyz</Trans>
+                </GlitchTitle>
+              </h1>
+              <p className="body-xl mx-auto mb-space-4 max-w-[40rem]">
+                <Trans t={t}>
+                  The link you are following is not hosted by Vega. You will be
+                  redirected in
+                </Trans>{' '}
+                {seconds}...
+              </p>
+              {url && (
+                <p className="body-xl">
+                  <a
+                    href={url}
+                    className="underline hover:no-underline"
+                    rel="noreferrer"
+                  >
+                    {url}
+                  </a>
+                </p>
+              )}
+            </div>
+          </Container>
         </div>
-      </Container>
+      )}
     </div>
   )
 }
