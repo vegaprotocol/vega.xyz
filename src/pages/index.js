124a3667974806243b0e87e93f2a34f6eb9a9841
Oliver Gosling <goslingo@gmail.com>
2023-01-17 14:16:30 +0000

    Fix broken link

--- src/pages/index.js
+++ src/pages/index.js
@@ -136,9 +136,7 @@ const IndexPage = ({ data }) => {
                   'Try out trading cash settled futures on the fully decentralised Vega network (Testnet).'
                 )}
                 icon={getImage(data.consoleIcon)}
-                link={routeThroughInterstitialPage(
-                  'https://console.fairground.wtf'
-                )}
+                link={'https://console.fairground.wtf'}
               />
               <ToolBox
                 title="Governance"
