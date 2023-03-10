7092d56409554f95802904d61aa2a4f989c65795
Penny Andrews <47143694+pennyandrews@users.noreply.github.com>
2023-03-10 16:22:45 +0000

    Update Mainnet Sims CTA (#495)
    * Update Mainnet Sims CTA
    
    Update Layout.js to update mainnet sims CTA
    
    * Update src/components/Layout.js
    
    align CTA with other apps

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -22,13 +22,13 @@ const Layout = ({ children, stickyHeader = true }) => {
             >
               <div>
                 <SiteBanner>
-                  The Mainnet Sims are live!{' '}
+                  Mainnet Sim 3 is coming soon!{' '}
                   <Link
                     to="https://fairground.wtf/"
                     className="font-bold underline hover:no-underline"
                   >
                     <br className="md:hidden" />
-                    Come help stress test the network
+                    Learn more
                   </Link>
                 </SiteBanner>
                 {children}
