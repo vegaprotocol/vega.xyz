96951a3a5186137c673fb9b3f705f15f7978bcf0
Ben-peoplemake <ben@peoplemake.io>
2023-03-17 20:02:44 +0000

    Revert "Update index.js"
    This reverts commit 0d29fd77dd90ef3decd07c124f7aaf223b569485.

--- src/pages/wallet/index.js
+++ src/pages/wallet/index.js
@@ -62,7 +62,7 @@ const DownloadButton = ({ binaries, title }) => {
         <div
           role="button"
           tabIndex={0}
-          className="flex items-center border border-current" data-cy={title}
+          className="flex items-center border border-current"
           onFocus={() => showDownloadMenu(true)}
           onBlur={(e) => {
             if (!e.relatedTarget?.dataset?.fileDownload) {
@@ -70,7 +70,7 @@ const DownloadButton = ({ binaries, title }) => {
             }
           }}
         >
-          <div className="border-px copy-xxs relative !mb-0 flex items-center py-3 pl-4 pr-6 text-center uppercase">
+          <div className="border-px copy-xxs relative !mb-0 flex items-center py-3 pl-4 pr-6 text-center uppercase" data-cy={title}>
             <div className="mr-4">
               <DropdownArrow />
             </div>
