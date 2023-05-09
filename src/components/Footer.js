9c90f452c8d862dce3db127988f88c159e816713
Dexter Edwards <dexter@vega.xyz>
2023-05-09 18:21:52 +0100

    Update src/components/Footer.js
    Co-authored-by: Penny Andrews <47143694+pennyandrews@users.noreply.github.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,7 +53,7 @@ const Footer = () => {
                 </li>
                 <li>
                   <TextLink
-                    to="https://governance.vega.xyz/proposals"
+                    to="https://governance.vega.xyz/"
                     className="block py-1.5 leading-[1.3]"
                   >
                     <Trans t={t}>Govern</Trans>
