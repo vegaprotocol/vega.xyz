616dfcf9ca0c1007abf6dab0e429e5fef955548c
ciaran- <ciaran@softbrew.io>
2023-05-09 15:28:50 +0100

    fixup description

--- src/components/VegaMarkets/Description.tsx
+++ src/components/VegaMarkets/Description.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export const Description = ({ market }: { market: any }) => {
-  console.log('market', market)
   const marketName = market.tradableInstrument.instrument.name
   const marketCode = market.tradableInstrument.instrument.code
   const symbol =
