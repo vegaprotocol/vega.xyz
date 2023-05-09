627a7e2f49475ad49101a32c06dab6dbd64bd73c
ciaran- <ciaran@softbrew.io>
2023-05-09 13:22:22 +0100

    fix: add horizontal padding around grid content

--- src/pages/markets/index.tsx
+++ src/pages/markets/index.tsx
@@ -42,7 +42,7 @@ const MarketsLiquidity = () => {
     <Layout stickyHeader={false}>
       <AsyncRenderer loading={loading} error={error} data={data}>
         <div
-          className="ag-theme-alpine w-full grow"
+          className="ag-theme-alpine relative w-full px-4 md:px-6 lg:px-8"
           style={{
             overflow: 'hidden',
           }}
