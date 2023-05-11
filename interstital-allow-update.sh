c5a701903795bf4a14488c8ecdb89f2d4499cfaf
Mikołaj Młodzikowski <mikolaj.mlodzikowski@gmail.com>
2023-05-11 18:31:51 +0200

    feat(ci): use proper hash in both files

--- interstital-allow-update.sh
+++ interstital-allow-update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -eux
 new_hash=$1
-echo "Updating hash to: $1"
+converted_hash=$(ipfs cid format -v 1 -b base32 $new_hash)
+echo "Updating hash to: $converted_hash"
 cat interstitial-allow.json \
-  | jq ".allowedIPFS[.allowedIPFS | length] |= . + \"$new_hash\"" -cM \
+  | jq ".allowedIPFS[.allowedIPFS | length] |= . + \"$converted_hash\"" -cM \
     > new-interstitial-allow.json
 jq . new-interstitial-allow.json > interstitial-allow.json
 rm new-interstitial-allow.json
-converted_hash=$(ipfs cid format -v 1 -b base32 $new_hash)
 sed -i "s|to = \"/external-link/?url=\"|to = \"/external-link/?url=$converted_hash\"|g" netlify.toml
