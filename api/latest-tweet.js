7e78e2df33d46371a831a582586f42886783e929
Edd <edd@vega.xyz>
2023-06-26 11:49:40 +0100

    chore: more logging

--- api/latest-tweet.js
+++ api/latest-tweet.js
@@ -20,13 +20,13 @@ exports.default = async (req, res) => {
   }
 
   const client = new Twitter({
-    consumer_key: process.env.TWITTER_API_KEY,
-    consumer_secret: process.env.TWITTER_API_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    consumer_key,
+    consumer_secret,
+    access_token_key,
+    access_token_secret,
   })
 
-  const { res } = await client.getCurrentUserV2Object()
+  console.dir(client)
 
   const { data, includes } = await client.get(
     `users/956831071982800896/tweets`,
