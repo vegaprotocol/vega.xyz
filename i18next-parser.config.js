65e76fcbc572e0e6308056127b9924d40039e62b
Oliver Gosling <goslingo@gmail.com>
2023-07-27 14:08:38 +0100

    feat: add vietnamese locale

--- i18next-parser.config.js
+++ i18next-parser.config.js
@@ -3,7 +3,7 @@ module.exports = {
   namespaceSeparator: false,
   saveMissing: true,
   skipDefaultValues: true,
-  locales: ['en', 'es', 'ru', 'cn', 'ko'],
+  locales: ['en', 'es', 'ru', 'cn', 'ko', 'vi'],
   lexers: {
     js: ['JsxLexer'],
     tsx: ['JsxLexer'],
