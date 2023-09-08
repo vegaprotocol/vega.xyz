bca64822c81dde51ca100c2b20a4c97ef03a0341
Oliver Gosling <goslingo@gmail.com>
2023-09-08 12:41:45 +0100

    feat: update mobile navigtaion

--- src/components/Navigation/MobileNavigation.js
+++ src/components/Navigation/MobileNavigation.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
+import About from './items/About'
 import Community from './items/Community'
 import Governance from './items/Governance'
+import Ecosystem from './items/Ecosystem'
 import MobileDropdown from './MobileDropdown'
-import { useTranslation } from 'gatsby-plugin-react-i18next'
+import Link from '../../components/UI/Link'
+import { Trans, useTranslation } from 'gatsby-plugin-react-i18next'
 
 const MobileNavigation = () => {
   const { t } = useTranslation('component.navigation')
@@ -19,20 +22,42 @@ const MobileNavigation = () => {
     <nav>
       <ul className="w-full max-w-[40rem] py-4 text-lg tracking-wide">
         <MobileDropdown
-          title={t('Community')}
+          title={t('About')}
+          id={1}
+          show={showId === 1}
+          onShow={handleShow}
+        >
+          <About />
+        </MobileDropdown>
+        <Link
+          to="https://docs.vega.xyz/"
+          className="font-not-glitched block py-3 text-[2.125rem] uppercase"
+        >
+          <Trans t={t}>Docs</Trans>
+        </Link>
+        <MobileDropdown
+          title={t('Governance')}
           id={2}
           show={showId === 2}
+          onShow={handleShow}
+        >
+          <Governance />
+        </MobileDropdown>
+        <MobileDropdown
+          title={t('Community')}
+          id={3}
+          show={showId === 3}
           onShow={handleShow}
         >
           <Community />
         </MobileDropdown>
         <MobileDropdown
-          title={t('Governance')}
-          id={3}
-          show={showId === 3}
+          title={t('Ecosystem')}
+          id={4}
+          show={showId === 4}
           onShow={handleShow}
         >
-          <Governance />
+          <Ecosystem />
         </MobileDropdown>
       </ul>
     </nav>
