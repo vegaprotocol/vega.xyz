d8ed3163c64f66fb7f0b764af23d0d2ab5f14115
Ciaran McGhie <ciaran@softbrew.io>
2023-10-18 12:49:38 +0100

    feat: add pill showing market type

--- src/components/VegaMarkets/Description.tsx
+++ src/components/VegaMarkets/Description.tsx
@@ -5,14 +5,24 @@ export const Description = ({ market }: { market: any }) => {
   const marketCode = market.tradableInstrument.instrument.code
   const symbol =
     market.tradableInstrument.instrument.product.settlementAsset.symbol
+  const marketType = market.tradableInstrument.instrument.product.__typename
+
   return (
-    <div style={{}}>
+    <div>
       {marketName}
       <br />
-      <div style={{ color: '#8B8B8B' }}>
-        {marketCode}
-        <br />
-        {symbol}
+      <div className="flex justify-between">
+        <div className="text-vega-dark-300">
+          {marketCode}
+          <br />
+          {symbol}
+          <br />
+        </div>
+        <div>
+          <div className="w-fit rounded-lg bg-vega-dark-200 p-1">
+            {marketType}
+          </div>
+        </div>
       </div>
     </div>
   )
