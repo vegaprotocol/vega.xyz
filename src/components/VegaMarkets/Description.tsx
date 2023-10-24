54aee2bf8be84ab2944b8df9c3ba4f3d08ad4499
ciaran- <ciaran@softbrew.io>
2023-10-24 14:40:42 +0100

    chore: move market type sticker to bottom of description block

--- src/components/VegaMarkets/Description.tsx
+++ src/components/VegaMarkets/Description.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
 
+const marketTypeToShortName = (marketType: string) => {
+  switch (marketType) {
+    case 'Future':
+      return 'FUTR'
+    case 'Perpetual':
+      return 'PERP'
+    case 'Spot':
+      return 'SPOT'
+    default:
+      return marketType
+  }
+}
+
 export const Description = ({ market }: { market: any }) => {
   const marketName = market.tradableInstrument.instrument.name
   const marketCode = market.tradableInstrument.instrument.code
@@ -11,19 +24,16 @@ export const Description = ({ market }: { market: any }) => {
     <div>
       {marketName}
       <br />
-      <div className="flex justify-between">
-        <div className="text-vega-dark-300">
-          {marketCode}
-          <br />
-          {symbol}
-          <br />
+      <div className="text-vega-dark-300">
+        {marketCode}
+        <br />
+        {symbol}
+        <div className="w-fit rounded-lg bg-vega-light-200 p-1 dark:bg-vega-dark-200">
+          {marketTypeToShortName(marketType)}
         </div>
-        <div>
-          <div className="w-fit rounded-lg bg-vega-light-200 p-1 dark:bg-vega-dark-200">
-            {marketType}
-          </div>
-        </div>
+        <br />
       </div>
+      <div></div>
     </div>
   )
 }
