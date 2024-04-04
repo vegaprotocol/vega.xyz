38a4a147b625bbce092f6fa093fc0399505d38d1
Oliver Gosling <goslingo@gmail.com>
2024-04-04 11:41:01 +0100

    updated partners and backers

--- src/components/PartnerBackers.tsx
+++ src/components/PartnerBackers.tsx
@@ -8,57 +8,68 @@ const PartnerBackers = () => {
   const partnerBackers = [
     {
       name: 'Pantera Capital',
-      link: 'https://veradiverdict.substack.com/p/decentralized-derivatives',
+      links: ['https://veradiverdict.substack.com/p/decentralized-derivatives'],
     },
     {
       name: 'Xpring',
-      link: 'https://coil.com/p/xpring/Investing-in-Vega-Protocol/cJsyyH9dq',
     },
-    { name: 'Hashed', link: '' },
-    { name: 'NGC Ventures', link: '' },
+    { name: 'Hashed' },
+    { name: 'NGC Ventures' },
     {
       name: 'gumi Cryptos Capital',
-      link: 'https://blog.gumi-cryptos.com/vc/why-gcc-invested-in-vega-protocol',
+      links: [
+        'https://blog.gumi-cryptos.com/vc/why-gcc-invested-in-vega-protocol',
+      ],
     },
     {
       name: 'RockawayX',
-      link: 'https://medium.com/rockaway-blockchain/rockaway-blockchain-announces-acquisition-of-a-stake-in-vega-a-protocol-for-decentralized-de4f4b7e3e09',
+      links: [
+        'https://medium.com/rockaway-blockchain/rockaway-blockchain-announces-acquisition-of-a-stake-in-vega-a-protocol-for-decentralized-de4f4b7e3e09',
+      ],
     },
     {
       name: 'KR1',
-      link: 'https://medium.com/@KR1/kr1-october-update-986f8614c9fe',
+      links: ['https://medium.com/@KR1/kr1-october-update-986f8614c9fe'],
     },
     {
       name: 'Eden Block',
-      link: 'https://www.edenblock.com/post/vega-protocol-investment-thesis',
+      links: ['https://www.edenblock.com/post/vega-protocol-investment-thesis'],
     },
     {
       name: 'Greenfield One',
-      link: 'https://medium.com/@greenfield_one/greenfield-one-backs-vega-to-develop-decentralized-derivatives-protocol-e1ec49f98cac',
+      links: [
+        'https://medium.com/@greenfield_one/greenfield-one-backs-vega-to-develop-decentralized-derivatives-protocol-e1ec49f98cac',
+        'https://www.arringtoncapital.com/wp-content/uploads/2021/06/vega_the_space_race_for_open_markets.pdf',
+      ],
     },
     {
       name: 'Monday Capital',
-      link: 'https://www.monday.capital/blockchain-startups/2019/10/5/vega-our-latest-investment',
+      links: [
+        'https://www.monday.capital/blockchain-startups/2019/10/5/vega-our-latest-investment',
+      ],
     },
-    { name: 'RSK Ecosystem Fund', link: '' },
-    { name: 'Zenith Ventures', link: '' },
+    { name: 'RSK Ecosystem Fund' },
+    { name: 'Zenith Ventures' },
     {
       name: 'Arrington Capital',
-      link: 'http://arringtonxrpcapital.com/2021/03/18/toward-credibly-neutral-derivatives-announcing-our-investment-into-vega/',
+      links: [
+        'http://arringtonxrpcapital.com/2021/03/18/toward-credibly-neutral-derivatives-announcing-our-investment-into-vega/',
+        'https://www.arringtoncapital.com/wp-content/uploads/2021/06/vega_the_space_race_for_open_markets.pdf',
+      ],
     },
-    { name: 'CMS Holdings', link: '' },
-    { name: 'CMT Digital', link: '' },
-    { name: 'Coinbase Ventures', link: '' },
-    { name: 'Cumberland DRW', link: '' },
-    { name: 'DeFi Alliance', link: '' },
-    { name: 'Loi Luu', link: '' },
-    { name: 'Mona El Isa', link: '' },
-    { name: 'ParaFi Capital', link: '' },
-    { name: 'SevenX Ventures', link: '' },
-    { name: 'Signum Capital', link: '' },
-    { name: 'Stani Kulechov', link: '' },
-    { name: 'Three Commas Capital', link: '' },
-    { name: 'Zee Prime Capital', link: '' },
+    { name: 'CMS Holdings' },
+    { name: 'CMT Digital' },
+    { name: 'Coinbase Ventures' },
+    { name: 'Cumberland DRW' },
+    { name: 'DeFi Alliance' },
+    { name: 'Loi Luu' },
+    { name: 'Mona El Isa' },
+    { name: 'ParaFi Capital' },
+    { name: 'SevenX Ventures' },
+    { name: 'Signum Capital' },
+    { name: 'Stani Kulechov' },
+    { name: 'Three Commas Capital' },
+    { name: 'Zee Prime Capital' },
   ]
 
   const Row = ({ member }) => {
@@ -66,17 +77,21 @@ const PartnerBackers = () => {
       <div className="border-b border-current py-2">
         <div className="flex justify-between gap-x-5">
           <div className="title-s">{member.name}</div>
-          {member.link ? (
-            <a
-              href={member.link}
-              rel="noreferrer"
-              className="title-xxs shrink-0  hover:underline"
-              target="_blank"
-            >
-              <Trans t={t}>What they say</Trans>
-              <LinkArrow className="ml-3 inline-block" />
-            </a>
-          ) : null}
+          <div>
+            {member.links &&
+              member.links.map((link, idx) => (
+                <a
+                  href={link}
+                  rel="noreferrer"
+                  className="title-xxs block shrink-0 !leading-normal hover:underline"
+                  target="_blank"
+                  key={idx}
+                >
+                  <Trans t={t}>What they say</Trans>
+                  <LinkArrow className="ml-3 inline-block" />
+                </a>
+              ))}
+          </div>
         </div>
       </div>
     )
